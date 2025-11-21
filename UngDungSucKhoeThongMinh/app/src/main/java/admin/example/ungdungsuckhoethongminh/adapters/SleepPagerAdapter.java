64872bdd0dc2385d68a39a7fdb8aaedb6a266a77
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-11-21 17:44:06 +0700

    update layout giấc ngủ

---
+++ UngDungSucKhoeThongMinh/app/src/main/java/admin/example/ungdungsuckhoethongminh/adapters/SleepPagerAdapter.java
@@ -0,0 +1,35 @@
+package admin.example.ungdungsuckhoethongminh.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import admin.example.ungdungsuckhoethongminh.fragments.SleepDayFragment;
+import admin.example.ungdungsuckhoethongminh.fragments.SleepMonthFragment;
+import admin.example.ungdungsuckhoethongminh.fragments.SleepWeekFragment;
+import admin.example.ungdungsuckhoethongminh.fragments.SleepYearFragment;
+
+public class SleepPagerAdapter extends FragmentStateAdapter {
+
+    public SleepPagerAdapter(@NonNull FragmentActivity fa) {
+        super(fa);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new SleepDayFragment();
+            case 1: return new SleepWeekFragment();
+            case 2: return new SleepMonthFragment();
+            case 3: return new SleepYearFragment();
+            default: return new SleepDayFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
