eca57e77b1f7b876b35ea76ab4bf6a3ba818be55
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2025-11-21 19:28:33 +0700

    quyen update layout ngày của giấc ngủ

--- UngDungSucKhoeThongMinh/app/src/main/java/admin/example/ungdungsuckhoethongminh/fragments/SleepDayFragment.java
+++ UngDungSucKhoeThongMinh/app/src/main/java/admin/example/ungdungsuckhoethongminh/fragments/SleepDayFragment.java
@@ -10,13 +10,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import admin.example.ungdungsuckhoethongminh.R;
+import admin.example.ungdungsuckhoethongminh.RecoveryArcView;
 
 public class SleepDayFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_sleep_day, container, false);
+
+        RecoveryArcView arc = view.findViewById(R.id.recoveryArc);
+        arc.setProgress(39);
 
         return view;
     }
